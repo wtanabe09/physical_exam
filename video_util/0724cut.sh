8101a32a444c8b856db599b35fef1654ef70c5ba
wtanabe09 <w.shoya.09@outlook.jp>
2023-08-19 02:58:31 +0900

    fix cut.sh

--- video_util/0724cut.sh
+++ video_util/0724cut.sh
@@ -1,8 +1,17 @@
 #! /bin/sh
 
+# chose a person "a or b or c"
+
 if [ ! -d cut_videos ]; then
     mkdir cut_videos
 fi
+
+# file name example a_001_手首.mp4
+names=(
+    "手首1"
+    "手首2"
+    "手首3"
+)
 
 times=(
     "19001910"
@@ -10,18 +19,10 @@ times=(
     "21412200"
 )
 
-# file_name: studentId_procedure, output_file_name: id_studentId_procedure
-names=("a_手首1" "a_手首2" "a_手首3")
-
-# echo ${times[0]}
-# echo ${names[0]}
 
 let i=0;
 for time in ${times[@]}; do
-    echo "${i}  ${time}"
-    echo "${i}  ${names[i-1]}"
     padding_i="00${i}"
-    echo "${padding_i:-3}"
-    python3 0724cut.py $time "${padding_i:-3}_${names[i]}"
+    python3 0724cut.py $time "a_${padding_i-3}_${names[i]}"
     let i++;
 done
