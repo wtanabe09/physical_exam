8f33759d7ff98744d16ade1abe5d0855926fb72e
wtanabe09 <w.shoya.0910@gmail.com>
2023-09-04 17:30:57 +0900

    rename output_video to feature_output

--- confidential/cut_videos/feature_output.py
+++ confidential/cut_videos/feature_output.py
@@ -2,14 +2,12 @@
 # from turtle import distance, window_width
 import cv2
 import sys
-import datetime
 import analysis # 自作 analysis.py
 
 
 file_base = sys.argv[1]
 input_file = './crop/'+file_base+'-C.mp4'
 window_name = 'display'
-delay = 1
 cap = cv2.VideoCapture(input_file)
 
 video_fps = cap.get(cv2.CAP_PROP_FPS)
@@ -20,8 +18,10 @@ fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 writer = cv2.VideoWriter('./feature_video/'+file_base+'-C_feature.mp4', fmt, video_fps, size)
 
 dist = analysis.calc_distance(file_base)
+int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 print(dist[0])
-print(video_fps)
+print(f'fps: {video_fps}')
+print(f'num of frame: {cap.get(cv2.CAP_PROP_FRAME_COUNT)}')
 
 i = 0
 while cap.isOpened():
@@ -36,7 +36,6 @@ while cap.isOpened():
         cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, thickness=2)
     
     writer.write(frame)
-
     i += 1
 
 cap.release()
