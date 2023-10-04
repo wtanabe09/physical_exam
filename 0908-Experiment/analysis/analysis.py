e5296caced534db5daeba2953ef7392e04db6157
wtanabe09 <w.shoya.09@outlook.jp>
2023-10-04 10:11:41 +0900

    fix elbow_angle.py

--- 0908-Experiment/analysis/analysis.py
+++ 0908-Experiment/analysis/analysis.py
@@ -2,10 +2,11 @@ import sys
 import math
 from matplotlib import pyplot
 import numpy as np
+import elbow_angle
 
 # 座標取得する際に左右反転されている．右手を取りたい場合，左手の座標を見る．
 input_csv = sys.argv[1]
-output_csv_path = sys.argv[2]
+result_csv_path = sys.argv[2]
 
 is_action = False
 row_counter = 0
@@ -13,8 +14,8 @@ count = 0
 action_number = 0
 threshold = 45
 
-output_arr = np.zeros(4) # real_time, video_time, hand_knee_distance, is_action
-output_csv = np.empty((0,4))
+result_arr = np.zeros(4) # real_time, video_time, hand_knee_distance, is_action
+result_csv = np.empty((0,4))
 
 with open(input_csv) as file:
   for line in file:
@@ -26,10 +27,16 @@ with open(input_csv) as file:
     x_doctor_right_knee = float(input_arr[(25*2)+2])
     y_doctor_right_knee = float(input_arr[(25*2)+2])
 
+    doctor_right_shoulder = np.array([float(input_arr[(11*2)+1]), float(input_arr[(11*2)+2])]) # 右肩（左肩インデックス）
+    doctor_right_elbow = np.array([float(input_arr[(13*2)+1]), float(input_arr[(13*2)+2])]) # 右肘
+    doctor_right_wrist = np.array([float(input_arr[(15*2)+1]), float(input_arr[(15*2)+2])]) # 右手首
+
     # 右手から右膝までの距離
     distance_hand_knee = math.sqrt((x_doctor_right_hand - x_doctor_right_knee)**2 + (y_doctor_right_hand - y_doctor_right_knee)**2)
     # print(round(row_counter/20, 1), round(distance_hand_knee, 1), end=', ')
     
+    elbow = elbow_angle.calc_elbow_angle(doctor_right_shoulder, doctor_right_elbow, doctor_right_wrist)
+
     if is_action:
       if distance_hand_knee <= threshold:
         # hand stay
@@ -56,12 +63,8 @@ with open(input_csv) as file:
     row_counter += 1
 
     # create output array for csv file
-    output_arr[0] = input_arr[0] # time into first col
-    output_arr[1] = round(row_counter/20, 1) # video seconds into first col
-    output_arr[2] = distance_hand_knee
-    output_arr[3] = is_action
+    result_arr = [input_arr[0], round(row_counter/20, 1), distance_hand_knee, elbow] # time into first col
+    result_csv = np.vstack((result_csv, result_arr))
 
-    output_csv = np.vstack((output_csv, output_arr))
-
-np.savetxt(output_csv_path, output_csv, delimiter = ',',fmt="%s")
+np.savetxt(result_csv_path, result_csv, delimiter = ',',fmt="%s")
 
--- 0908-Experiment/analysis/elbow_angle.py
+++ 0908-Experiment/analysis/elbow_angle.py
@@ -6,19 +6,30 @@ import numpy as np
 input_csv = sys.argv[1] #csv ファイルセレクト
 # output_csv_path = sys.argv[2]
 
-with open(input_csv) as file:
-  for line in file:
-    input_arr = line.split(",")
-
-    # 右肩（左肩インデックス）
-    doctor_right_shoulder = np.array([float(input_arr[11*2]+1), float(input_arr[11*2]+2)])
-    # 右肘
-    doctor_right_elbow = np.array([float(input_arr[(13*2)+1]), float(input_arr[(13*2)+2])])
-    # 右手首
-    doctor_right_wrist = np.array([float(input_arr[(15*2)+1]), float(input_arr[(15*2)+2])])
-    calc_elbow_angle(doctor_right_shoulder, doctor_right_elbow, doctor_right_wrist)
-
 
 def calc_elbow_angle(shoulder, elbow, wrist):
   elbow_to_shoulder = shoulder - elbow # ベクトル
   elbow_to_wrist = wrist - elbow # ベクトル
+
+  dot_product = np.dot(elbow_to_shoulder, elbow_to_wrist)
+
+  norm_elbow_to_shoulder = np.linalg.norm(elbow_to_shoulder)
+  norm_elbow_to_wrist = np.linalg.norm(elbow_to_wrist)
+
+  cos_theta = dot_product / (norm_elbow_to_shoulder * norm_elbow_to_wrist)
+  angle_radians = np.arccos(np.clip(cos_theta, -1.0, 1.0))
+
+  # ラジアンから度に変換
+  angle_degrees = math.degrees(angle_radians)
+  
+  return 180 - angle_degrees
+
+# with open(input_csv) as file:
+#   for line in file:
+#     input_arr = line.split(",")
+
+#     doctor_right_shoulder = np.array([float(input_arr[(11*2)+1]), float(input_arr[(11*2)+2])]) # 右肩（左肩インデックス）
+#     doctor_right_elbow = np.array([float(input_arr[(13*2)+1]), float(input_arr[(13*2)+2])]) # 右肘
+#     doctor_right_wrist = np.array([float(input_arr[(15*2)+1]), float(input_arr[(15*2)+2])]) # 右手首
+
+#     print(calc_elbow_angle(doctor_right_shoulder, doctor_right_elbow, doctor_right_wrist))
