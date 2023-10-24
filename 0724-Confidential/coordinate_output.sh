438db0eee885a3ef8e14669e7d51016e3f6b2f41
wtanabe09 <w.shoya.09@outlook.jp>
2023-10-24 16:14:19 +0900

    rename dir

--- 0724-Confidential/coordinate_output.sh
+++ 0724-Confidential/coordinate_output.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # input=$@ # clop
-files=`ls crop/c_*-C.mp4`
+files=`ls crop_videos/c_*-C.mp4`
 echo $files
 for file in $files; do
   base=`basename $file .mp4`
   echo $base
-  python3 coordinate_output.py "./crop/${base}.mp4" "./point_csv/${base}.csv"
+  python3 coordinate_output.py "./crop_videos/${base}.mp4" "./point_csv/${base}.csv"
 done
 
 echo prosess is done
